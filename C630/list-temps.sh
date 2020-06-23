233906366241f5f931b8df19c23473599aea969d
bm16ton <bm16ton@gmail.com>
2020-06-22 20:55:29 -0400

    made the read temps script more human readable and added fahrenheit conversion

--- C630/list-temps.sh
+++ C630/list-temps.sh
@@ -2,5 +2,7 @@
 for i in $(seq 0 19) ; do
 TYPE=$(cat /sys/class/thermal/thermal_zone$i/type)
 TEMP=$(cat /sys/class/thermal/thermal_zone$i/temp)
-echo "$TYPE = $TEMP"
+FTEMP=$(echo "$TEMP / 1000" |bc)
+FTEMP2=$(echo "scale=2;((9/5) * $FTEMP) + 32" |bc)
+echo "$TYPE = $FTEMP"C" $FTEMP2"F""
 done 
