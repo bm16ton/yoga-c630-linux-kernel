34080317b475e79bc7c695d4b15a119c015b842d
bm16ton <bm16ton@gmail.com>
2020-06-24 09:46:49 -0400

    channel cleanup

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -38,7 +38,7 @@
 #define ATH10K_FLUSH_TIMEOUT_HZ (5 * HZ)
 #define ATH10K_CONNECTION_LOSS_HZ (3 * HZ)
 #define ATH10K_NUM_CHANS 42
-#define ATH10K_MAX_5G_CHAN 180
+#define ATH10K_MAX_5G_CHAN 177
 
 /* Antenna noise floor */
 #define ATH10K_DEFAULT_NOISE_FLOOR -95
