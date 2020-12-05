fd7106ecbee4c4207283dab9372a5929a2cb097e
bm16ton <bm16ton@gmail.com>
2020-12-05 02:11:05 -0500

    added 2 scripts for generating .hex files to run signed crda db inside kernel, C630/add-crda-pem-hex.sh works, the other one C630/add-pem-hex.sh doesnt add a comma to end, and probly more

--- C630/add-crda-pem-hex.sh
+++ C630/add-crda-pem-hex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 {
-    for cert in /lib/crda/pubkeys/root.x509.pem; do
+    for cert in /lib/crda/pubkeys/*.x509.pem; do
         openssl x509 -in $cert -outform der;
     done
-} | hexdump -v -e '1/1 "0x%.2x," "\n"' > net/wireless/certs/root.hex
+} | hexdump -v -e '1/1 "0x%.2x," "\n"' > net/wireless/certs/$cert.hex
