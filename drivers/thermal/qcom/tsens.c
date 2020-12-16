dfbdb92cf9929170538b5dceb2a46abfcb86a4f7
bm16ton <bm16ton@gmail.com>
2020-12-16 07:51:14 -0500

    sdm845 add hwmon tsense connector

--- drivers/thermal/qcom/tsens.c
+++ drivers/thermal/qcom/tsens.c
@@ -17,6 +17,8 @@
 #include <linux/regmap.h>
 #include <linux/slab.h>
 #include <linux/thermal.h>
+#include "../thermal_hwmon.h"
+#include "../thermal_core.h"
 #include "tsens.h"
 
 /**
@@ -973,6 +975,12 @@ static int tsens_register(struct tsens_priv *priv)
 		priv->sensor[i].tzd = tzd;
 		if (priv->ops->enable)
 			priv->ops->enable(priv, i);
+	
+	tzd->tzp->no_hwmon = false;
+    ret = thermal_add_hwmon_sysfs(tzd);
+    if (ret)
+    return ret;  
+           
 	}
 
 	ret = tsens_register_irq(priv, "uplow", tsens_irq_thread);
